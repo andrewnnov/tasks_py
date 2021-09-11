b6245985d6f9d51009f5194870cc0df650e9505a
andreybannikov <andrewnnov@yandex.ru>
2021-09-11 09:16:28 +0300

    revers (bad working solution)

--- leetcode/reverse_integer.py
+++ leetcode/reverse_integer.py
@@ -8,7 +8,8 @@ import math
 
 
 def reverse_int(x: int):
-    if x >= 2 ** 31 -1 and x <= -2 ** 31 and x == 0:
+
+    if x == 0:
         return 0
 
     if x > 0:
@@ -24,7 +25,34 @@ def reverse_int(x: int):
 
         print(list_of_number)
         new_number = int("".join(list_of_number))
-        return new_number
 
 
-print(reverse_int(123456789))
+        if new_number >= 2 ** 31 - 1 or new_number <= -2 ** 31 - 1:
+            return 0
+        else:
+            return new_number
+
+
+    else:
+        number_in_str = str(x)
+        list_of_number = []
+        list_of_number[:0] = number_in_str[1:]
+        print(list_of_number)
+
+        for i in range(0, len(list_of_number)//2):
+            temp = list_of_number[i]
+            list_of_number[i] = list_of_number[len(list_of_number) - 1 - i]
+            list_of_number[len(list_of_number) - 1 - i] = temp
+
+        print(list_of_number)
+        new_number = int("".join(list_of_number))
+
+        if -2 ** 31 <= new_number <= 2 ** 31 - 1:
+            return 0
+        else:
+            return -new_number
+
+
+print(reverse_int(1534236469))
+
+
